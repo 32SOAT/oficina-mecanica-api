4cdaec5059ac01b045ba61c189e0697a0b1545ac
JulianaAmoriN <juamorim.sn@gmail.com>
2026-06-15 19:38:56 -0300

    feat(clientes): refatorando clean architecture removendo outros modulos que ainda nao foram refatorados

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,27 +8,14 @@ import { type ConfigType, appConfigSchema } from './config/config.types';
 import { typeOrmConfig } from './config/database.config';
 import { jwtConfig } from './config/jwt.config';
 import { TypedConfigService } from './config/typed-config.service';
-import { ClienteEntity } from './clientes/cliente.entity';
 import { ClienteModule } from './clientes/cliente.module';
-import { EstoqueEntity } from './estoque/estoque.entity';
-import { EstoqueModule } from './estoque/estoque.module';
-import { ServicoEntity } from './servicos/servico.entity';
-import { ServicoModule } from './servicos/servico.module';
 import { UserEntity } from './users/user.entity';
 import { UserModule } from './users/user.module';
-import { VeiculoEntity } from './veiculos/veiculo.entity';
-import { VeiculoModule } from './veiculos/veiculo.module';
-import { OrdemServicoEntity } from './ordens-de-servico/ordem-servico.entity';
-import { ItemOsServicoEntity } from './ordens-de-servico/entities/item-os-servico.entity';
-import { ItemOsEstoqueEntity } from './ordens-de-servico/entities/item-os-estoque.entity';
-import { HistoricoStatusOsEntity } from './ordens-de-servico/entities/historico-status-os.entity';
-import { OrdemServicoModule } from './ordens-de-servico/ordem-servico.module';
 import { APP_INTERCEPTOR } from '@nestjs/core';
 import { TransformResponseInterceptor } from './interceptors/transform-response.interceptor';
 import { QueryingModule } from './querying/querying.module';
-import { SeedingModule } from './database/seeding/seeding.module';
-import { shouldEnableSeedingModule } from './database/seeding/seeding-environment';
 import { AuthModule } from './auth/auth.module';
+import { ClienteTypeormEntity } from './clientes/infra/typeorm/cliente.typeorm.entity';
 
 @Module({
   imports: [
@@ -43,17 +30,7 @@ import { AuthModule } from './auth/auth.module';
 
         return {
           ...databaseConfig,
-          entities: [
-            ClienteEntity,
-            ServicoEntity,
-            VeiculoEntity,
-            EstoqueEntity,
-            UserEntity,
-            OrdemServicoEntity,
-            ItemOsServicoEntity,
-            ItemOsEstoqueEntity,
-            HistoricoStatusOsEntity,
-          ],
+          entities: [UserEntity, ClienteTypeormEntity],
         };
       },
     }),
@@ -66,14 +43,9 @@ import { AuthModule } from './auth/auth.module';
       isGlobal: true,
     }),
     ClienteModule,
-    EstoqueModule,
-    ServicoModule,
     UserModule,
-    VeiculoModule,
-    OrdemServicoModule,
     AuthModule,
     QueryingModule,
-    ...(shouldEnableSeedingModule(process.env.NODE_ENV) ? [SeedingModule] : []),
   ],
   controllers: [AppController],
   providers: [
